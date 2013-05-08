2843e4023d704b89f6ca79caf8926b4cf384c455
Jon <jon.r.mcclure@gmail.com>
2013-05-07 20:14:25 -0500

    cat ready for drop

--- nonprofits/balance_sheets/views.py
+++ nonprofits/balance_sheets/views.py
@@ -2,6 +2,9 @@ from balance_sheets.models import Revenue_Expense
 from django.shortcuts import render_to_response
 
 def index(request):
+
+	categories=Revenue_Expense.objects.values('category').distinct()
+
 	all_line_items=Revenue_Expense.objects.all()
 
-	return render_to_response('index.html', {'all_line_items':all_line_items})
+	return render_to_response('index.html', {'categories':categories,  'all_line_items':all_line_items})
